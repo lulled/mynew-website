292a201b6263421c21a87d1abef67b77d518301e
lule ahmed <ahmedlule31@gmail.com>
2022-07-23 21:34:01 +0300

    fixed brand image with .block class

--- src/componets/Brand.js
+++ src/componets/Brand.js
@@ -16,7 +16,7 @@ function Brand() {
                     <p className='text-slate-700 mt-5 py- md:mx-20  text-[20px]  font-light font-poppins '>
                    Spin-up your own Brand and practically sell anything, whether you're just starting out or a mature business we gonna get u covered.
                     </p>
-                    <img src={Brand1} alt='' className='hidden md:visible  md:w-full md:justify-center md:items-center   '/>
+                    <img src={Brand1} alt='' className='hidden md:block  md:w-full md:justify-center md:items-center   '/>
                     <img src={mbc} alt='' className=' w-full justify-center items-center  md:hidden '/>
                 </div>
 
